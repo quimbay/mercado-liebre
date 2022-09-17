a775f42d5ce1da83e41e1d9b6aa1e072f772f41f
quimbay <julianapineroz@gmail.com>
2022-09-17 15:51:13 -0500

    archivos app

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static("public"));
+
+app.listen(process.env.PORT  || 3000, function(){
+    console.log("servidor corriendo en el puerto 3000");
+});
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname,"./views/home.html"));
+});
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+})
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+})
+
+app.post('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+})
